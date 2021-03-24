00b0d191ffb1f194cd38253d5ea1dd49aa30d4ec
Andra729 <szucsandras37@gmail.com>
2021-03-24 21:39:32 +0100

    for mobile 24-03-2021 21:40

---
+++ script.js
@@ -0,0 +1,81 @@
+/************************************************
+ * Positioning function
+ ************************************************/
+function imgPostitions () {
+    var x = Math.floor((Math.random() *400));
+    var y = Math.floor((Math.random() *400));
+
+    return {x, y};
+};
+
+/************************************************
+ * LeftSide smiling faces generator
+ ************************************************/
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById('leftSide');
+
+function generateFaces() {
+    theLeftSide.innerHTML = "";
+
+    for (var i = 0; i < numberOfFaces; i++) {
+        
+        var imgElement = document.createElement('IMG');
+
+        imgElement.src = "smile.png";
+        imgElement.style.top = imgPostitions().y + "px";
+        imgElement.style.left = imgPostitions().x + "px";
+
+        theLeftSide.appendChild(imgElement);
+    }
+    /* csalókód */
+    console.log(theLeftSide.lastChild );
+ };
+
+onload = generateFaces();
+
+ /************************************************
+ * Cloning the left-1 to RightSide
+ ************************************************/
+var theRightSide = document.getElementById('rightSide');
+
+function cloneGeneration() {
+
+    theRightSide.removeChild(theRightSide.firstChild)
+    var cloned = theLeftSide.cloneNode(true);
+    theRightSide.appendChild(cloned).removeChild(cloned.lastChild);
+}
+
+cloneGeneration();
+
+ /************************************************
+ * ONCLICK event
+ ************************************************/
+theLeftSide.lastChild.onclick = function nextLevel(event){
+    while ( !theLeftSide.lastChild.onclick ) {   
+        event.stopPropagation();
+        numberOfFaces += 5;
+
+        generateFaces();
+        cloneGeneration();}
+};
+
+
+ /************************************************
+ * BODYCLICK event
+ ************************************************/
+var theBody = document.getElementsByTagName('body')[0];
+
+theBody.onclick = function gameOver() {
+
+    alert("Game Over!\n\n" + numberOfFaces/5 +". level");
+
+    theBody.onclick = null;
+
+    theLeftSide.lastChild.onclick = null;
+};
+
+ /************************************************
+ * Repetition
+ ************************************************/
+/*
+Faszom tudja, már mindent kipróbáltam...*/
